c2904e2b79b784b271641ceae2a9119795b37848
Trenk74 <robertbeslija@gmail.com>
2021-11-21 14:44:47 +0100

    dodato state->localStorage funkcionalnost. za sada radi ;)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import { debounce } from 'debounce';
 import App from './App';
+
+import { saveState } from './store/browser-storage';
 import { theme } from './components/UI/globalTheme';
 import store from './store/store';
 
-import { ToastContainer } from 'react-toastify';
+store.subscribe(
+	debounce(() => {
+		saveState(store.getState());
+	}, 800)
+);
 
 ReactDOM.render(
 	<React.StrictMode>
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+
+import { loadState } from './browser-storage';
 import authReducer from './authSlice';
 
 const store = configureStore({
 	reducer: { auth: authReducer },
+	preloadedState: loadState(),
 });
 
 export default store;
